0fb50001cd272ea4e8d938ffae0eb8e603f6eea9
gustavo <gustorn.gdf@gmail.com>
2024-09-24 20:16:12 -0300

    v2

--- static/js/cart.js
+++ static/js/cart.js
@@ -10,7 +10,28 @@ for (i = 0; i < updateBtns.length; i++) {
         if(user === 'AnonymousUser'){
             console.log('Não logado')
         }else{
-            console.log('logado, enviando dados')
+            updateUserOrder(productId, action)
         }
     })
+}
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+		var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
 }
